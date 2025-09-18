b3ce0e0c01cbc2b7b89bd5e8569963ea7c0a18fb
Facundo Zamora <facu.m.zamoratu@gmail.com>
2025-09-18 20:02:20 -0300

    Agregar server.js con Helmet y ruta /_api/package.json

---
+++ server.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+const express = require('express');
+const helmet = require('helmet');
+const fs = require('fs');
+const app = express();
+
+// Oculta el encabezado "X-Powered-By: Express"
+app.use(helmet.hidePoweredBy());
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.send('Mi app de Node.js está corriendo en Render 🚀');
+});
+
+// Ruta que devuelve package.json para FreeCodeCamp
+app.get('/_api/package.json', (req, res) => {
+  fs.readFile(__dirname + '/package.json', 'utf8', (err, data) => {
+    if (err) return res.status(500).send(err);
+    res.type('json').send(data);
+  });
+});
+
+// Puerto dinámico de Render
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Servidor escuchando en puerto ' + PORT);
+});
+
+module.exports = app;
